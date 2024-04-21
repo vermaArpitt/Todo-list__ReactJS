402beabce632f06d4efbfbbd80c2fe87fb4c09ab
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-04-22 01:26:34 +0530

    Basic react app with some front end

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './MyComponents/Header';
+import Footer from './MyComponents/Footer';
+import Todos from './MyComponents/Todos';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Todos/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
